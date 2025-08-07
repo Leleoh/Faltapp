a8f1c3df82bd2047a24a0c30c3bf01da7f909234
Leonel <leonelfhernandez@outlook.com>
2025-08-06 21:25:33 -0300

    Commit inicial

---
+++ Faltapp/FaltappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaltappApp.swift
+//  Faltapp
+//
+//  Created by Leonel Ferraz Hernandez on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FaltappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
